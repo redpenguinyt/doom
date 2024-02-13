ebd812a0fdf3c7ea020e6f781ce3caa687762b45
redpenguinyt <redpenguin777@yahoo.com>
2024-02-13 08:58:43 +0000

    temporarily removed auto-resize code

--- src/renderer.rs
+++ src/renderer.rs
@@ -59,11 +59,12 @@ impl Renderer {
 
     pub fn draw(&mut self, context: &mut Context) -> Result<(), String> {
         // TODO: avoid mutating context
-        self.canvas
-            .window_mut()
-            .set_size(VSCREEN_WIDTH * PIXEL_SCALE, VSCREEN_HEIGHT * PIXEL_SCALE)
-            .map_err(|e| e.to_string())?;
-        self.draw_background()?;
+        // self.canvas
+        //     .window_mut()
+        //     .set_size(VSCREEN_WIDTH * PIXEL_SCALE, VSCREEN_HEIGHT * PIXEL_SCALE)
+        //     .map_err(|e| e.to_string())?;
+
+        self.draw_background();
 
         self.draw_3d(context)?;
 
